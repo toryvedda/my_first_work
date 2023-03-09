66bb8b743fd647e776886a1ff4e4418df9634b4f
tory <gigris@mail.ru>
2023-03-09 19:53:25 +0300

    Poprobuem

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -120,7 +120,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Мои настройки.
 LOGIN_URL = '/users/login/'
-
-# Настройки Heroku
-import django_heroku
-django_heroku.settings(locals())
